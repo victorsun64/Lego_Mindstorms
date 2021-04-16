ec4778dad8ad07241316e64c1edc8ee2675bd41d
victo <victo@DESKTOP-AQBOHOH>
2021-04-16 18:08:58 +0200

    Java File erstellt

---
+++ src/lego_Mindstorms/Lego_Mindstorms.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package lego_Mindstorms;
+public class Lego_Mindstorms {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
