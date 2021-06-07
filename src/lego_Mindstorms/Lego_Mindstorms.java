e90f2bde7df136a55d32d386774cb337ceada512
victor.sun <victor.sun@AMS-GYM-EDV1-04.gym.ams.local>
2021-06-07 17:10:55 +0200

    Lego_Mindstorms

--- src/lego_Mindstorms/Lego_Mindstorms.java
+++ src/lego_Mindstorms/Lego_Mindstorms.java
@@ -8,12 +8,7 @@ import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.SensorPort;
 import lejos.robotics.RegulatedMotor;
 import lejos.robotics.SampleProvider;
-import lejos.utility.Delay;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
-
-import org.jfree.util.Rotation;
-
-import lejos.hardware.Button;
 
 public class Lego_Mindstorms {
 
@@ -22,6 +17,8 @@ public class Lego_Mindstorms {
 	 */
 	public static void main(String[] args) {
 		// crack.com
+
+
 		RegulatedMotor rechts = new EV3LargeRegulatedMotor(MotorPort.A);
 		RegulatedMotor links = new EV3LargeRegulatedMotor(MotorPort.B);
 		RegulatedMotor rgabel = new EV3LargeRegulatedMotor(MotorPort.C);
@@ -29,24 +26,30 @@ public class Lego_Mindstorms {
 		EV3UltrasonicSensor sonic = new EV3UltrasonicSensor(SensorPort.S1);
 		final SampleProvider sp = sonic.getDistanceMode();
 		int distanceValue = 0;
+	
+			final int distancethreshold = 10000;
+			for (int i = 0; i <= distancethreshold; i++) {
 
-		if (Button.ENTER.isDown()) {
-			
-	        final int distancethreshold = 1000;
-	        for(int i = 0; i <= distancethreshold; i++) {
-
-	        	float [] sample = new float[sp.sampleSize()];
-	            sp.fetchSample(sample, 0);
-	            distanceValue = (int)sample[0];
-
-				if ( distanceValue < 0.1 ) {lgabel.backward();
-				rgabel.backward();}
+				float[] sample = new float[sp.sampleSize()];
+				sp.fetchSample(sample, 0);
+				distanceValue = (int) sample[0];
 				
-	     
+				if (distanceValue < 0.1) {
+					lgabel.backward();
+					rgabel.backward();
+				} else  {
+					rechts.forward();
+					links.forward();
+				}
+
 			}
 			;
-		}
-		;
+
+			rechts.close();
+			links.close();
+			lgabel.close();
+			rgabel.close();
+			sonic.close();
 	}
 
 }
