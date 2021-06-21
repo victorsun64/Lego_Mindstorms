ab8556ab9aaf1a9a3a2aa4211f04d4b857c534eb
victor.sun <victor.sun@AMS-GYM-EDV1-04.gym.ams.local>
2021-06-21 16:57:23 +0200

    github hochgeladen

--- src/lego_Mindstorms/Lego_Mindstorms.java
+++ src/lego_Mindstorms/Lego_Mindstorms.java
@@ -8,9 +8,15 @@ import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.SensorPort;
 import lejos.robotics.RegulatedMotor;
 import lejos.robotics.SampleProvider;
-import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.hardware.sensor.EV3IRSensor;
 
 public class Lego_Mindstorms {
+
+	private static RegulatedMotor rechts;
+	private static RegulatedMotor links;
+	private static RegulatedMotor rgabel;
+	private static RegulatedMotor lgabel;
+	private static EV3IRSensor sonic;
 
 	/**
 	 * @param args
@@ -18,38 +24,31 @@ public class Lego_Mindstorms {
 	public static void main(String[] args) {
 		// crack.com
 
-		RegulatedMotor rechts = new EV3LargeRegulatedMotor(MotorPort.A);
-		RegulatedMotor links = new EV3LargeRegulatedMotor(MotorPort.B);
-		RegulatedMotor rgabel = new EV3LargeRegulatedMotor(MotorPort.C);
-		RegulatedMotor lgabel = new EV3LargeRegulatedMotor(MotorPort.D);
-		EV3UltrasonicSensor sonic = new EV3UltrasonicSensor(SensorPort.S2);
+		rechts = new EV3LargeRegulatedMotor(MotorPort.A);
+		links = new EV3LargeRegulatedMotor(MotorPort.B);
+		rgabel = new EV3LargeRegulatedMotor(MotorPort.C);
+		lgabel = new EV3LargeRegulatedMotor(MotorPort.D);
+		sonic = new EV3IRSensor(SensorPort.S4);
 		final SampleProvider sp = sonic.getDistanceMode();
-		int distanceValue = 0;
-		final int distancethreshold = 100;
-		for (int i = 0; i <= distancethreshold; i++) {
+		while(true) {
 
 			float[] sample = new float[sp.sampleSize()];
 			sp.fetchSample(sample, 0);
-			distanceValue = (int) sample[0];
+			int distanceValue = (int) sample[0];
 
-			while (distanceValue > 0.3) {
+			while (distanceValue > 30) {
 				links.forward();
 				rechts.forward();
 			}
 			;
-			while (distanceValue < 0.3) {
+			while (distanceValue < 30) {
 				rgabel.backward();
 				lgabel.backward();
 			}
 
 		}
-		;
-
-		rechts.close();
-		links.close();
-		lgabel.close();
-		rgabel.close();
-		sonic.close();
+		
+		
 	}
 
 }
